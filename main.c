44d49c0876f4d85c2439e743a9398ce1aa4de308
Lol_bn@lol <Lol_bn@lol.com>
2017-03-05 17:23:13 +0200

    1

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+
+{
+
+	int sum, days, res, dohod;
+	printf("Insert sum, please: ");
+	scanf("%d", &sum);
+	printf("\nInsert days, please: ");
+	scanf("%d", &days);
