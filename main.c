42982e94fc35c72836635abf1dbdd808b8505612
Lol_bn@lol <Lol_bn@lol.com>
2017-03-05 17:32:43 +0200

    2

--- main.c
+++ main.c
@@ -9,3 +9,25 @@ int main()
 	scanf("%d", &sum);
 	printf("\nInsert days, please: ");
 	scanf("%d", &days);
+        while(sum<10000)
+
+	{
+		printf("\nFailed! Insert please sum not less than 10,000: ");
+		scanf("%d", &sum );
+	}
+	while(days>365)
+	{
+		printf("\nFailed! Insert please days no more than 365: ");
+		scanf("%d", &days );
+	}
+	if(sum<=100000) //Ðàñ÷žò ïðèáûëè ïðè ñóììå äî 100.000
+	{
+		if((days>=0)&&(days<=30))
+			res=sum-(sum/100*10);
+		if((days>=31)&&(days<=120))
+			res=sum+(sum/100*2);
+		if((days>=121)&&(days<=240))
+			res=sum+(sum/100*6);
+		if((days>=241)&&(days<=365))
+			res=sum+(sum/100*15);
+	}
